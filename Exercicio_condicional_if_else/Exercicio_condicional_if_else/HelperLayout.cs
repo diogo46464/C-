44c05f1d5e39d8b4eaba54d65aefb76743fc0da2
Diogo Pincetta <diogohe08@gmail.com>
2025-05-28 22:28:29 -0300

    Exercicio Condicional If e Else

---
+++ Exercicio_condicional_if_else/Exercicio_condicional_if_else/HelperLayout.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Exercicio_condicional_if_else {
+    public class HelperLayout {
+
+        public static void ExerciseNumber(int number) {
+            Console.WriteLine("|---------------------------------------------------------|");
+            Console.WriteLine($"|-------------------Iniciando exercícios {number}----------------|");
+            Console.WriteLine("|---------------------------------------------------------|");
+        }
+
+        public static void Separation() { Console.WriteLine("\n\n|------------------------------------------------|\n\n"); }
+    }
+}
