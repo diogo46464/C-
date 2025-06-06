7bf4a3c3f80e0505283b66f0778d43ba20b4fa4c
Diogo Pincetta <diogohe08@gmail.com>
2025-06-05 23:01:11 -0300

    Exercicios adicionados

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿using ConsoleApp1;
+
+Exercise.ex1();
---
+++ Exercise_While/Exercise_While/Template.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Exercise_While {
+    public class Template {
+        public static void model1(int number) {
+            Console.WriteLine("|---------------------|");
+            Console.WriteLine($"|--------Exercise{number}------------|");
+        }
+
+        public static void model2() {
+            Console.WriteLine("|\n\n-------------------------------------\n\n|");
+        }
+    }
+}
