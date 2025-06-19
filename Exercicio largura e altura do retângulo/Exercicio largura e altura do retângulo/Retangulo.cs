2fdeeb6ae90ac1620dab4bd28b8ce3ec66a5d5d4
Diogo Pincetta <diogohe08@gmail.com>
2025-06-19 14:08:18 -0300

    Exercicios com classes, atributos e métodos

---
+++ Exercicio largura e altura do retângulo/Exercicio largura e altura do retângulo/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exercicio_largura_e_altura_do_retângulo {
+    internal class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+
+            double A = Largura * Altura;
+            return A;
+        }
+
+        public double Perimetro() {
+
+            double P = 2 * (Largura + Altura);
+
+            return P;
+        }
+
+        public double Diagonal() {
+
+            double D = Math.Sqrt((Largura * Largura) + (Altura * Altura));
+
+            return D;
+        }
+
+    }
+}
