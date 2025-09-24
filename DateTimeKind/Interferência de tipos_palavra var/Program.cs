d9592a4673098c5f6a514c01c89f814c3b318795
Diogo Pincetta <diogohe08@gmail.com>
2025-09-23 21:57:40 -0300

    DateTimeKind e ISO 8601

---
+++ DateTimeKind/Interferência de tipos_palavra var/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+
+DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+
+DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+
+Console.WriteLine("d1: " + d1);
+Console.WriteLine("d1 Kind: " + d1.Kind);
+Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+Console.WriteLine();
+Console.WriteLine("d1: " + d2);
+Console.WriteLine("d1 Kind: " + d2.Kind);
+Console.WriteLine("d1 to Local: " + d2.ToLocalTime());
+Console.WriteLine("d1 to Utc: " + d2.ToUniversalTime());
+Console.WriteLine();
+Console.WriteLine("d1: " + d3);
+Console.WriteLine("d1 Kind: " + d3.Kind);
+Console.WriteLine("d1 to Local: " + d3.ToLocalTime());
+Console.WriteLine("d1 to Utc: " +  d3.ToUniversalTime());
